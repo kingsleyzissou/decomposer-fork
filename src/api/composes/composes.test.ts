4d4cafc4347c24c3d4e1d0435e6d56809874bda4
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-08-29 16:09:54 +0100

    api/composes: add POST /compose endpoint
    Add the initial boilerplate for creating a compose. The handler will
    push the compose to the worker queue, where a worker will process the
    compose request.

--- src/api/composes/composes.test.ts
+++ src/api/composes/composes.test.ts
@@ -3,7 +3,9 @@ import { StatusCodes } from 'http-status-codes';
 import { mkdtemp, rmdir } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import path from 'node:path';
+import { validate } from 'uuid';
 
+import { composeRequest } from '@fixtures';
 import { createTestClient, createTestStore } from '@fixtures';
 
 import type { Composes } from './types';
@@ -25,5 +27,24 @@ describe('Composes handler tests', async () => {
     expect(body.meta.count).toBe(0);
     expect(body.data).not.toBeUndefined();
     expect(body.data.length).toBe(0);
+  });
+
+  it('POST /compose should create a new compose', async () => {
+    const res = await client.compose.$post({
+      json: composeRequest,
+    });
+    expect(res.status).toBe(StatusCodes.OK);
+    const { id } = await res.json();
+    expect(validate(id)).toBeTrue();
+  });
+
+  it('GET /composes should have one compose now', async () => {
+    const res = await client.composes.$get();
+    expect(res.status).toBe(StatusCodes.OK);
+    const body = (await res.json()) as Composes;
+    expect(body).not.toBeUndefined();
+    expect(body.meta.count).toBe(1);
+    expect(body.data).not.toBeUndefined();
+    expect(body.data.length).toBe(1);
   });
 });
--- src/api/composes/index.ts
+++ src/api/composes/index.ts
@@ -4,7 +4,8 @@ import Maybe from 'true-myth/maybe';
 import type { AppContext } from '@app/types';
 
 import { paginate } from '../pagination';
-import type { Compose, Composes } from './types';
+import type { Compose, ComposeId, Composes } from './types';
+import * as validators from './validators';
 
 export const composes = new Hono<AppContext>()
 
@@ -22,6 +23,27 @@ export const composes = new Hono<AppContext>()
         return ctx.json<Composes>(
           paginate<Compose>(composes, Maybe.of(limit), Maybe.of(offset)),
         );
+      },
+      Err: (error) => {
+        const { body, code } = error.response();
+        return ctx.json(body, code);
+      },
+    });
+  })
+
+  /**
+   * curl --unix-socket /run/decomposer-httpd.sock \
+   * -H "Content-Type: application/json" \
+   * -d @src/__fixtures__/compose.json \
+   * -X POST 'http://localhost/api/image-builder-composer/v2/compose'
+   */
+  .post('/compose', validators.createCompose, async (ctx) => {
+    const { compose: service } = ctx.get('services');
+    const result = await service.add(ctx.req.valid('json'));
+
+    return result.match({
+      Ok: ({ id }) => {
+        return ctx.json<ComposeId>({ id });
       },
       Err: (error) => {
         const { body, code } = error.response();
