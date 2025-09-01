8a8a9b3dede0815df532ebf0766602ce5eaef1dd
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-09-01 17:09:23 +0100

    adsfadf

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,13 @@ import { notFound, onError } from '@app/errors';
 
 const middleware = new Hono()
   .notFound(notFound)
-  .onError(onError)
   .use(prettyJSON())
   .use(logger());
 
 export const app = new Hono()
+  // we need to catch the errors either at the router
+  // level or the app level, otherwise the errors go
+  // uncaught
+  .onError(onError)
   .route('*', middleware)
   .get('/health', (c) => c.json({ message: 'OK', ok: true }, 200));
